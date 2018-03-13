da0b2cad29646c5ea9d6248a6c669082748db3e0
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-03-13 12:29:38 -0400

    Disable TLS
    Setting TLS to true does not allow us to connect to the IRC server

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
+
 	"github.com/thoj/go-ircevent"
 )
 
 const channel = "#bookz"
-const serverssl = "irc.undernet.org:6667"
+const serverssl = "172.106.10.18:6667"
 
 func main() {
-	ircnick1 := "zeeus___"
-	irccon := irc.IRC(ircnick1, "zeeus___")
+	ircnick1 := "blatiblat"
+	irccon := irc.IRC(ircnick1, "IRCTestSSL")
 	irccon.VerboseCallbackHandler = true
 	irccon.Debug = true
-	irccon.UseTLS = true
-	irccon.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	// irccon.UseTLS = true
+	// irccon.TLSConfig = &tls.Config{InsecureSkipVerify: false}
 	irccon.AddCallback("001", func(e *irc.Event) { irccon.Join(channel) })
 	irccon.AddCallback("366", func(e *irc.Event) {})
 	err := irccon.Connect(serverssl)
